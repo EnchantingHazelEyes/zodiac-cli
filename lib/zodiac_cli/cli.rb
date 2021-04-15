d7f7f22ac8a7bd9c020ba27b0f938432a4234fcf
Enchanting121 <samantha.barros95@gmail.com>
2021-04-14 23:32:43 -0400

    Beginning stages with first input

--- lib/zodiac_cli/cli.rb
+++ lib/zodiac_cli/cli.rb
@@ -1,3 +1,17 @@
 class CLI
     
+    def start 
+        puts "Welcome to the world of Zodiacs"
+        intro 
+        input = user_input
+    end
+    
+    def intro
+        puts "Please enter your birthday in MM/DD format to find your Zodiac!"
+    end
+
+    def user_input
+        gets.strip
+    end
+
 end
--- lib/zodiac_cli/zodiac.rb
+++ lib/zodiac_cli/zodiac.rb
@@ -1,5 +1,24 @@
 class Zodiac
-    attr_accessor :zodiac_name , 
+    attr_accessor :name , :birthday , :dates
+
+    @@all = []
+
+    def initialize(name)
+        @zodiac_name = [] 
+        @name = name
+        @birthday = birthday
+        @dates = dates
+        save
+    end
+
+    def save 
+        @@all << self 
+    end
+
+    def self.all
+        @@all
+    end
+
 
 
 end
