e6620ee7ceefcdece97422c8fd6c911364d57153
Enchanting121 <samantha.barros95@gmail.com>
2021-04-16 18:51:30 -0400

    next stage

--- lib/zodiac_cli/cli.rb
+++ lib/zodiac_cli/cli.rb
@@ -4,6 +4,7 @@ class CLI
         puts "Welcome to the world of Zodiacs"
         intro 
         input = user_input
+
     end
     
     def intro
@@ -13,5 +14,29 @@ class CLI
     def user_input
         gets.strip
     end
+
+    def options 
+        response = user_input
+        
+        if response =  
+     #if date is valid, display zodiac information
+        #if want another zodiac, enter in new date
+     #if exit is entered , application quits
+     #sif invalid answer given, prompt for another answer
+    end
+
+    def invalid_response1
+        puts "Incorrect response, Please enter date in MM/DD format"
+    end
+
+    def exit_program
+        puts "Hope you enjoyed learning about your Zodiac! Come Again!"
+    end
+
+    def zodiac_info
+
+    end
+
+
 
 end
