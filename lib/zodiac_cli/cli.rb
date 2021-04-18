f51d86be728a3ac7b3b16783ce449f552125a715
Enchanting121 <samantha.barros95@gmail.com>
2021-04-18 12:19:02 -0400

    Cli basics done

--- lib/zodiac_cli/cli.rb
+++ lib/zodiac_cli/cli.rb
@@ -3,26 +3,34 @@ class CLI
     def start 
         puts "Welcome to the world of Zodiacs"
         intro 
-        input = user_input
+        #input = user_input
+        options
 
     end
     
     def intro
-        puts "Please enter your birthday in MM/DD format to find your Zodiac!"
+        puts "Please enter a birthday in MM/DD format to find a Zodiac!"
+        puts "or enter exit to exit the program!"
     end
 
     def user_input
         gets.strip
     end
 
+    #@possible_dates = [12/01]
+
     def options 
         response = user_input
-        
-        if response =  
-     #if date is valid, display zodiac information
-        #if want another zodiac, enter in new date
-     #if exit is entered , application quits
-     #sif invalid answer given, prompt for another answer
+        if response == "12/01"     #if date is valid, display zodiac name
+            zodiac_info
+            deeper_info      #provides deeper information for your zodiac
+
+        elsif response == "exit" #if exit is entered , application quits
+            exit_program
+        else
+            invalid_response1 #if invalid answer given, prompt for another answer
+            options
+        end
     end
 
     def invalid_response1
@@ -34,8 +42,27 @@ class CLI
     end
 
     def zodiac_info
+        zodiac_names = ["caps", "Aquarius", "sags"]
+       # zodiac_names.each do |zodiac|
+        puts zodiac_names
+        end
+    
+   def deeper_info #provides deeper information on your zodiac
+        puts "If you want to know more about your zodiac, enter 'y', if you wish to exit, enter 'exit'"
+        response = user_input
+        if response == "y" 
+            puts "Zodiac information"
+        elsif response == "exit"
+            exit_program
+        else
+            invalid_response2
+            deeper_info
+        end 
+    end 
 
-    end
+   def invalid_response2
+        puts "Invalid response, enter 'y' to see more information, or 'exit' to exit application"
+   end
 
 
 
